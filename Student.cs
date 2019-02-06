0d959347ce754bc345849e3495d77daae32886bf
GuptaRaman12 <47058957+GuptaRaman12@users.noreply.github.com>
2019-02-06 13:24:21 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCSharpprgs
+{
+    class Student
+    {
+        private string studentName;
+        private string studentId;
+        public string StudentName
+        {
+            get => studentName;
+            set => studentName = value;
+        }
+        public string StudentId
+        {
+            get => studentId;
+            set => studentId = value;
+        }
+    }
+}
